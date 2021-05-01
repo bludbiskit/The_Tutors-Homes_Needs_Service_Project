1d9c0d79d6a87654c8cbb9a39329a8efd87831e9
bludbiskit <32342990+bludbiskit@users.noreply.github.com>
2021-05-01 00:13:29 -0500

    Update signInController.php

--- Code/php_code/signInController.php
+++ Code/php_code/signInController.php
@@ -15,99 +15,57 @@ if ($conn->connect_error) {
 }
 
 echo "Connected successfully";
-$email = $_POST['email'];
-$password = $_POST['password'];
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
-$phone = $_POST['phone'];
-$address = $_POST['address'];
-$zip = $_POST['zip'];
-$city = $_POST['city'];
-$state = $_POST['state'];
-$license = $_POST['license'];
+$email = cleanString($_POST['email']);
+$password = cleanString($_POST['password']);
+echo $email;
 
-$_SESSION['firstName'] = $firstName;
-$_SESSION['lastName'] = $lastName;
-$_SESSION['phone'] = $phone;
-$_SESSION['email'] = $email;
-$_SESSION['password'] = $password;
-$_SESSION['streetAddress'] = $address;
-$_SESSION['zip'] = $zip;
-$_SESSION['city'] = $city;
-$_SESSION['state'] = $state;
-$_SESSION['licenseNumber'] = $license;
-
-$sql = "SELECT * from users where email='$email'";
-$result = $conn->query($sql);
+$s = "SELECT * from users where email='$email'";
+echo $s;
+$result = $conn->query($s);
+$row = $result->fetch_row();
 if ($result->num_rows > 0) {
-    echo phpAlert("Email Already In Use!", "signUp.php");
-}
-else{
-    $status = $statusMsg = ''; 
-    if(isset($_POST['submit'])){ 
-        $status = 'error';
-        if(!empty($_FILES["profilePicture"]["name"]) && !empty($_FILES["driversLicensePicture"]["name"])) { 
-            // Get file info 
-            $fileName1 = basename($_FILES["profilePicture"]["name"]); 
-            $fileName2 = basename($_FILES["driversLicensePicture"]["name"]); 
-    
-            $fileType1 = pathinfo($fileName1, PATHINFO_EXTENSION); 
-            $fileType2 = pathinfo($fileName2, PATHINFO_EXTENSION); 
-            // Allow certain file formats 
-            $allowTypes = array('jpg','png','jpeg','gif'); 
-    
-            if(in_array($fileType1, $allowTypes) && in_array($fileType2, $allowTypes)){ 
-                $image1 = $_FILES['profilePicture']['tmp_name']; 
-                $image2 = $_FILES['driversLicensePicture']['tmp_name'];
-                $imgContent1 = addslashes(file_get_contents($image1)); 
-                $imgContent2 = addslashes(file_get_contents($image2));
-
-                $_SESSION['licensePicture'] = $imgContent2;
-                $_SESSION['profilePicture'] = $imgContent1;
-
-                // Insert image content into database 
-                $insert = $conn->query("INSERT INTO users VALUES (DEFAULT,'$firstName','$lastName','$phone','$email','$password','$address','$zip','$city','$state','$license','$imgContent2','$imgContent1')"); 
-                $sql = "SELECT User_ID from users where email='$email'";
-                $result = $conn->query($sql);
-                $row = $result->fetch_row();
-                $distance = $_POST['distance'];
-
-                $_SESSION['distance'] = $distance;
-
-                if(isset($_POST['check_list'])){
-                    $sql = "INSERT into servicers values (DEFAULT,'$row[0]','$distance',0)";
-                    $conn->query($sql);
-                    $sql = "SELECT Servicer_ID from servicers where User_ID='$row[0]'";
-                    $result = $conn->query($sql);
-                    $servicer_ID = $result->fetch_row();
-                    $serv = $_POST['check_list'];
-
-                    $_SESSION['services'] = $serv;
-
-                    for ($x = 0; $x < count($_POST['check_list']); $x++) {
-                        $sql = "INSERT into providesservice values (DEFAULT,'$servicer_ID[0]','$serv[$x]')";
-                        $conn->query($sql);
-                      }
-                }
-                else{
-                    $sql = "INSERT into clients values (DEFAULT,'$row[0]')";
-                    $conn->query($sql);
-                }
-                if($insert){ 
-                    $status = 'success'; 
-                    $statusMsg = "File uploaded successfully."; 
-                }else{ 
-                    $statusMsg = "File upload failed, please try again."; 
-                }  
-            }else{ 
-                $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
-            } 
-        }else{ 
-            $statusMsg = 'Please select an image file to upload.'; 
-        } 
-    } 
-    echo $statusMsg; 
+  if ($password != $row[5]) {
+    echo phpAlert("Incorrect Password", "signIn.php");
+  }
+  else {
+    $sql = "SELECT Servicer_ID, Distance_From_Address from servicers where User_ID='$row[0]'";
+    $result = $conn->query($sql);
+    $servicer_ID = $result->fetch_row();
+    if(isset($servicer_ID)){
+      $sql = "SELECT Service from providesservice where Servicer_ID='$servicer_ID[0]'";
+      $result = $conn->query($sql);
+      $x = 0;
+      while($thing = $result->fetch_row()) {
+        $_SESSION['services'][$x] = $thing[0];
+        $_SESSION['distance'] = $thing[1];
+        $x = $x + 1;
+      }
+      //echo $_SESSION['distance'];
+    }
+    $_SESSION['firstName'] = $row[1];
+    $_SESSION['lastName'] = $row[2];
+    $_SESSION['phone'] = $row[3];
+    $_SESSION['email'] = $row[4];
+    $_SESSION['password'] = $row[5];
+    $_SESSION['streetAddress'] = $row[6];
+    $_SESSION['zip'] = $row[7];
+    $_SESSION['city'] = $row[8];
+    $_SESSION['state'] = $row[9];
+    $_SESSION['licenseNumber'] = $row[10];
+    $_SESSION['licensePicture'] = $row[11];
+    $_SESSION['profilePicture'] = $row[12];
     header('location:main.php');
+    exit();
+  }
+  // output data of each row
+  /*
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["Email"]."<br>";
+  }
+  */
+}
+else {
+  echo phpAlert("Unrecognized Email", "signIn.php");
 }
 $conn->close();
 ?>
